a098f032ec5176d4a5e0e3edc97d3fe73e0cacc3
shivasuryan-vummidi <svummidi@stuy.edu>
2017-04-03 11:16:15 -0400

    Update Deque.java

--- Deque.java
+++ Deque.java
@@ -1,15 +1,27 @@
 public interface Deque<Card>{
-
+    /*PRECONDITION: You have a valid element and a valid Deque
+    POSTCONDITION: Add the element to the start of the Deque.
+    */
     public void addFirst(Card x);
-
+    /*PRECONDITION: You have a valid element and a valid Deque
+    POSTCONDITION: Add the element to the end of the Deque.
+    */
     public void addLast(Card x);
-
+    /*PRECONDITION: You have a valid Deque
+    POSTCONDITION: Removes the first element from the Deque
+    */
     public Card removeFirst();
-
+    /*PRECONDITION: You have a valid Deque
+    POSTCONDITION: Removes the last element from the Deque
+    */
     public Card removeLast();
-
+    /*PRECONDITION: You have a valid Deque and is not empty
+    POSTCONDITION: Return the first element of the Deque
+    */
     public Card peekFirst();
-
+    /*PRECONDITION: You have a valid Deque and is not empty
+    POSTCONDITION: Return the last element of the Deque
+    */
     public Card peekLast();
 
 
